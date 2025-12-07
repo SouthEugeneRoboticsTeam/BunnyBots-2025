9ff212f42a40f145f5c4587ddc96e6c46933868a
Drive 2 - Bartholomew <benji.urbancic@gmail.com>
2025-12-07 13:17:17 -0800

    Added inputs for gunner and driver

--- src/main/java/org/sert2521/bunnybots2025/Input.kt
+++ src/main/java/org/sert2521/bunnybots2025/Input.kt
@@ -1,6 +1,7 @@
 package org.sert2521.bunnybots2025
 
 import dev.doglog.DogLog
+import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.wpilibj.GenericHID
@@ -9,7 +10,12 @@ import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
+import org.sert2521.bunnybots2025.commands.VisionAlign
 import org.sert2521.bunnybots2025.subsystems.drivetrain.Drivetrain
+import org.sert2521.bunnybots2025.subsystems.flywheels.FlywheelsSubsystem
+import org.sert2521.bunnybots2025.subsystems.indexer.IndexerSubsystem
+import org.sert2521.bunnybots2025.subsystems.intake.IntakeSubsystem
+import org.sert2521.bunnybots2025.subsystems.wrist.WristSubsystem
 import yams.motorcontrollers.SmartMotorController
 import yams.motorcontrollers.SmartMotorControllerConfig
 
@@ -17,16 +23,33 @@ object Input {
     private val driverController = CommandXboxController(0)
     private val gunnerController = CommandJoystick(1)
 
-    private val exampleDriverButton = driverController.x()
-    private val exampleGunnerButton = gunnerController.button(2)
+    private val driverRev = driverController.leftBumper()
+    private val driverShoot = driverController.rightBumper()
+    private val driverAlign = driverController.a()
+    private val gunnerIntake = gunnerController.button(1)
+    private val gunnerReverse = gunnerController.button(2)
+    private val gunnerStow = gunnerController.button(4)
+    private val gunnerRunIntake = gunnerController.button(10)
+    private val gunnerLowerWrist = gunnerController.button(5)
 
     private val resetRotOffset = driverController.y()
 
     private var rotationOffset = Rotation2d(0.0)
 
     init {
-        exampleDriverButton.whileTrue(Commands.none())
-        exampleGunnerButton.onTrue(Commands.none())
+        driverRev.onTrue(FlywheelsSubsystem.rev())
+        driverRev.onFalse(FlywheelsSubsystem.stop())
+        driverShoot.onTrue(IndexerSubsystem.kick())
+        driverAlign.onTrue(VisionAlign(RobotConstants.targetVisionPose))
+        gunnerIntake.onTrue(WristSubsystem.toIntake()
+            .andThen(IntakeSubsystem.runIntake()))
+        gunnerIntake.onFalse(WristSubsystem.toStow()
+            .andThen(IntakeSubsystem.stop()))
+        gunnerReverse.whileTrue(IntakeSubsystem.runReverse())
+        gunnerStow.onTrue(WristSubsystem.toStow()
+            .andThen(IntakeSubsystem.stop()))
+        gunnerRunIntake.whileTrue(IntakeSubsystem.runIntake())
+        gunnerLowerWrist.onTrue(WristSubsystem.toIntake())
 
         resetRotOffset.onTrue(Commands.runOnce({ rotationOffset = Drivetrain.getPose().rotation }))
     }
